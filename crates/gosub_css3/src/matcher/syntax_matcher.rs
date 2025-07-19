e184eb1c0a141466dafdcf473bd2b7239a4f3648
mysangle <mysangle@users.noreply.github.com>
2025-07-19 17:54:25 +0900

    check a unit of '__qem' when converting to px

--- crates/gosub_css3/src/matcher/syntax_matcher.rs
+++ crates/gosub_css3/src/matcher/syntax_matcher.rs
@@ -15,9 +15,9 @@ pub struct MatchResult<'a> {
 }
 
 #[allow(dead_code)]
-const LENGTH_UNITS: [&str; 31] = [
+const LENGTH_UNITS: [&str; 32] = [
     "cap", "ch", "em", "ex", "ic", "lh", "rcap", "rch", "rem", "rex", "ric", "rlh", "vh", "vw", "vmax", "vmin", "vb",
-    "vi", "cqw", "cqh", "cqi", "cqb", "cqmin", "cqmax", "px", "cm", "mm", "Q", "in", "pc", "pt",
+    "vi", "cqw", "cqh", "cqi", "cqb", "cqmin", "cqmax", "px", "cm", "mm", "Q", "in", "pc", "pt", "__qem",
 ];
 
 /// A CSS Syntax Tree is a tree sof CSS syntax components that can be used to match against CSS values.
